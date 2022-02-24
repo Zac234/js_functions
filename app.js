31db782e900084bbd1408363e3afeb7c6f06a486
zac234 <zacbenincasa@gamil.com>
2022-02-24 09:32:37 -0700

    this all should work

---
+++ app.js
@@ -0,0 +1,25 @@
+function add(number){
+    var number= 3 +9
+    return number
+}
+
+console.log(add)
+
+
+var numbers = "1 2 3 4 5 6 7 8 9 1 2 "
+
+function greater_than(numbers){
+    if (numbers >= 10){
+        return true
+    }else if (numbers< 10 ){
+        return false
+    }else{
+        console.log("math sucks")
+    }
+} 
+
+function get(ph){
+    
+var notanarray= ("phishing","fishing","fish")
+console.log (notanarray.filter(ph))
+}
